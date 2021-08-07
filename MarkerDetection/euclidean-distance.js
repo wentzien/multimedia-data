bdc8763329f819186c4b30a94427d21e6ec7662e
larslutz96 <larslutz96@web.de>
2021-08-07 18:07:52 +0200

    github

--- MarkerDetection/euclidean-distance.js
+++
@@ -1,6 +0,0 @@
-const color1 = [213, 205, 226];
-const color2 = [212, 195, 226];
-
-// euclidean distance
-const distance = Math.sqrt((color1[0] - color2[0]) ** 2 + (color1[1] - color2[1]) ** 2 + (color1[2] - color2[2]) ** 2);
-console.log(distance);
